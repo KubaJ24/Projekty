699f2f24b8d145fc7c10ce074b56c8ea9fecff5a
KubaJ24 <j.jarosz@onet.pl>
2023-07-22 13:25:29 +0200

    Wersja I odczytu ADC z potencjometru

--- Projekt inzynierski II/projekt_inz_ii/funkcje.cpp
+++ Projekt inzynierski II/projekt_inz_ii/funkcje.cpp
@@ -4,16 +4,17 @@
 #include "driver/timer.h"
 #include "driver/adc.h"
 #include "esp_adc_cal.h"
+#include <Arduino.h>
 
 #include "funkcje.h"
 
 #define silnik_L GPIO_NUM_18
 #define silnik_P GPIO_NUM_19
 #define silnik_PWM GPIO_NUM_21
-//#define silnik_pot ADC1_CHANNEL_x                   //x - numer gpio
+#define silnik_pot ADC1_CHANNEL_7                 //GPIO 32 - 39 - tutaj GPIO 35
 
 void test_monitor(){
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void gpio_config(){
@@ -21,6 +22,9 @@ void gpio_config(){
   gpio_set_direction(silnik_L, GPIO_MODE_OUTPUT); gpio_set_level(silnik_L, 0);
   gpio_set_direction(silnik_P, GPIO_MODE_OUTPUT); gpio_set_level(silnik_P, 0);
   gpio_set_direction(silnik_PWM, GPIO_MODE_OUTPUT); gpio_set_level(silnik_PWM, 0);
+
+  //Potencjometr sterujący silnikiem
+  gpio_set_direction(GPIO_NUM_35, GPIO_MODE_INPUT);
 }
 
 void adc_config(){
@@ -30,7 +34,7 @@ void adc_config(){
   //Dzielnik to potencjometr 10 kOhm i identyczny rezystor
   //Rozdzielczosc 10 bit -> PWM jest 10 bit
   esp_adc_cal_characteristics_t adc;
-  esp_adc_cal_characterize( ADC_UNIT_1, ADC_ATTEN_DB_11, ADC_WIDTH_BIT_10, 1100, &adc);
+  esp_adc_cal_characterize( ADC_UNIT_1, ADC_ATTEN_DB_2_5, ADC_WIDTH_BIT_10, 1100, &adc);
 }
 
 uint16_t odczyt_adc(){
--- Projekt inzynierski II/projekt_inz_ii/projekt_inz_ii.ino
+++ Projekt inzynierski II/projekt_inz_ii/projekt_inz_ii.ino
@@ -17,20 +17,20 @@ DO ZROBIENIA
 #include "driver/timer.h"
 #include "driver/adc.h" 
 #include "esp_adc_cal.h"
+#include <Arduino.h>
 
 #include "funkcje.h"
 
-#define silnik_L GPIO_NUM_18
-#define silnik_P GPIO_NUM_19
-#define silnik_PWM GPIO_NUM_21
-//#define silnik_pot ADC1_CHANNEL_x                   //x - numer gpio, GPIO 32 - 39
+uint16_t wyn = 0;
 
 void setup() { 
   test_monitor();
   gpio_config();
-
+  adc_config();
 }
 
 void loop() {
-  
+  wyn = odczyt_adc();
+  Serial.println(wyn);
+  vTaskDelay(250 / portTICK_RATE_MS);
 }
