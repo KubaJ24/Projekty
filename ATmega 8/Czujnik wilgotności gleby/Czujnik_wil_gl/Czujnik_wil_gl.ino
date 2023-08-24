248be671b5d2574937801d28d31142aa782600fe
KubaJ24 <j.jarosz@onet.pl>
2023-08-24 12:35:37 +0200

    Aktualizacja programu czujnika wilgotności. Dodano funkcje "konwersjaADC"

--- ATmega 8/Czujnik wilgotności gleby/Czujnik_wil_gl/Czujnik_wil_gl.ino
+++ ATmega 8/Czujnik wilgotności gleby/Czujnik_wil_gl/Czujnik_wil_gl.ino
@@ -1,14 +1,14 @@
 #include "my_sev_seg.h"
-#include <Wire.h>
+#include "konwersjaADC.h"
 #include <Arduino.h>
 
-int a = 0;
-int b = 0;
+uint8_t a = 0;
+uint8_t b = 0;
 int liczba = 0;
 int wynik = 0;
 
-int* dz = &a;
-int* jed = &b;
+uint8_t* dz = &a;
+uint8_t* jed = &b;
 
 void setup() {
   //Wyjścia
@@ -20,10 +20,16 @@ void setup() {
   PORTD = 0x00;
   PORTB = 0x00;
   PORTC = 0x00;
+
+  //Konfiguracja ADC
+  adc_config();
 }
 
 void loop() {
-  wynik = analogRead(PC0);
+  adc_start();
+  while((ADCSRA & (1 << ADSC)) == 1){}
+  wynik = adc_result();
+  // 0 - 670 -> zakres wskazań czujnika dla 5V Vcc
   liczba = map(wynik, 0, 670, 0, 99);
   number_extract(liczba, dz, jed);
   number_display(dz, jed);
